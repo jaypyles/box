56b89c4f78e62bf2398b418bd4de546993575b19
Jayden Pyles <jpylesbusiness@gmail.com>
2024-12-20 20:18:08 -0600

    fix: media move

--- src/box/utils/yaml/utils.py
+++ src/box/utils/yaml/utils.py
@@ -2,14 +2,16 @@ import yaml
 from typing import Any
 import os
 
-DEFAULT_CONFIG_PATH = "$HOME/.config/box"
+DEFAULT_CONFIG_PATH = f"{os.getenv('HOME')}/.config/box"
 
 
 def load_config(path: str) -> Any:
     os.makedirs(DEFAULT_CONFIG_PATH, exist_ok=True)
 
     if not os.path.exists(f"{DEFAULT_CONFIG_PATH}/{path}.yaml"):
-        raise FileNotFoundError(f"Config file {path} not found")
+        raise FileNotFoundError(
+            f"Config file {DEFAULT_CONFIG_PATH}/{path}.yaml not found"
+        )
 
     with open(f"{DEFAULT_CONFIG_PATH}/{path}.yaml", "r") as file:
         return yaml.safe_load(file)
