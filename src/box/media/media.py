e0ba30151a0d1bb3b053bd3863003eb7b02545ff
Jayden Pyles <jpylesbusiness@gmail.com>
2024-12-21 22:00:33 -0600

    fix: media move

--- src/box/media/media.py
+++ src/box/media/media.py
@@ -28,18 +28,13 @@ def list_downloaded_files(config: MediaConfig):
 
 def determine_type_of_file():
     type_of_file = console.input(
-        """Is this a:
-        [cyan][1] Episode File[/cyan]
-        [cyan][2] Movie File[/cyan]
-        [cyan][3] Movie Folder[/cyan]
-        [cyan][4] Season Folder[/cyan]
-        [cyan]Enter the number of the type of file: [/cyan]"""
+        """Is this a:\n[cyan][1] Episode File[/cyan]\n[cyan][2] Movie File[/cyan]\n[cyan][3] Movie Folder[/cyan]\n[cyan][4] Season Folder[/cyan]\n[cyan]Enter the number of the type of file: [/cyan]"""
     )
 
     return type_of_file
 
 
-def list_inner_media_folder(media_path: str):
+def list_season_folder(media_path: str):
     media_dirs = os.listdir(media_path)
 
     for idx, dir in enumerate(media_dirs):
@@ -48,12 +43,20 @@ def list_inner_media_folder(media_path: str):
     print(f"Select a season to move the episode to: ")
 
     season_idx = console.input("Enter the number of the season: ")
-    season_dir = media_dirs[int(season_idx) - 1]
+
+    if not season_idx:
+        return media_path
+
+    try:
+        season_dir = media_dirs[int(season_idx) - 1]
+    except (IndexError, ValueError):
+        _ = execute_command(f"mkdir {media_path}/{season_idx}")
+        season_dir = season_idx
 
     return season_dir
 
 
-def list_media_folder(config: MediaConfig, media_type: str):
+def list_show_folder(config: MediaConfig, media_type: str):
     media_path = config.get(media_type + "_path", "")
     if not media_path or not os.path.isdir(media_path):
         print(f"[red]Error: Invalid path for media type '{media_type}'.[/red]")
@@ -71,21 +74,43 @@ def list_media_folder(config: MediaConfig, media_type: str):
         _ = execute_command(f"mkdir {media_path}/{show_idx}")
         show_dir = show_idx
 
-    season_dir = list_inner_media_folder(f"{media_path}/{show_dir}")
+    season_dir = list_season_folder(f"{media_path}/{show_dir}")
 
     return season_dir
 
 
-def move_episode(config: MediaConfig, selected_file: str):
-    list_media_folder(config, "tv")
+def list_movie_folder(config: MediaConfig, media_type: str):
+    media_path = config.get(media_type + "_path", "")
+
+    if not media_path or not os.path.isdir(media_path):
+        print(f"[red]Error: Invalid path for media type '{media_type}'.[/red]")
+        return
+
+    movie_dirs = os.listdir(media_path)
+
+    for idx, dir in enumerate(movie_dirs):
+        print(f"[cyan][{idx + 1}] {dir}[/cyan]")
+
+    selected_movie_idx = console.input("Select a movie folder to move to: ")
+    try:
+        selected_movie = movie_dirs[int(selected_movie_idx) - 1]
+    except (IndexError, ValueError):
+        _ = execute_command(f"mkdir {media_path}/{selected_movie_idx}")
+        selected_movie = selected_movie_idx
+
+    return selected_movie
 
 
-def move_movie(config: MediaConfig, selected_file: str):
-    list_media_folder(config, "movie")
+def move_episode(config: MediaConfig):
+    return list_show_folder(config, "tv")
 
 
-def move_season(config: MediaConfig, selected_file: str):
-    list_media_folder(config, "tv")
+def move_movie(config: MediaConfig):
+    return list_movie_folder(config, "movie")
+
+
+def move_season(config: MediaConfig):
+    return list_show_folder(config, "tv")
 
 
 def move_download_to_media():
@@ -95,9 +120,21 @@ def move_download_to_media():
 
     selected_file = list_downloaded_files(config)
 
+    destination_dir = None
+
     if media_type == "tv":
-        move_episode(config, selected_file)
+        destination_dir = move_episode(config)
+
     elif media_type == "movie":
-        move_movie(config, selected_file)
+        destination_dir = move_movie(config)
+        if destination_dir:
+            for file in os.listdir(selected_file):
+                file_path = f"{selected_file}/{file}"
+                _ = execute_command(
+                    f"sudo mv {shlex.quote(file_path)} {shlex.quote(destination_dir)}"
+                )
+
     elif media_type == "season":
-        move_season(config, selected_file)
+        destination_dir = move_season(config)
+
+    # execute_command(f"mv {selected_file} {destination_dir}")
